52462bfd7af15e55bb253cc85dbe89d247472ef9
First commit <antoniozzanolli@gmail.com>
2022-04-06 16:51:30 -0300

    Adicionar arquivos de projeto.

---
+++ Carros/ContentContext/Carro.cs
@@ -0,0 +1,39 @@
+﻿namespace Carros.ContentContext
+{
+    internal class Carro
+    {
+        public bool situacao = false;
+
+        public int distancia = 0;
+        public string Nome { get; set; }
+        public int Peso{ get; set; }
+        public string Modelo { get; set; }
+
+        public Carro(string nome,int peso,string modelo)
+        {
+            Nome = nome;
+            Peso = peso;
+            Modelo = modelo; 
+        }
+
+        public void andar()
+        {
+            this.distancia++;
+        }
+
+        public void re()
+        {
+            this.distancia--;
+        }
+
+        public void ligar()
+        {
+            this.situacao = true;
+        }
+
+        public void desligar()
+        {
+            this.situacao = false;
+        }
+    }
+}
---
+++ Carros/Program.cs
@@ -0,0 +1,20 @@
+﻿using Carros.ContentContext;
+
+var carro = new Carro("Sandero", 1000, "Renault");
+carro.ligar();
+
+Console.WriteLine(carro.situacao);
+
+carro.andar();
+carro.andar();
+carro.andar();
+
+carro.re();
+
+carro.desligar();
+
+Console.WriteLine(carro.situacao);
+Console.WriteLine(carro.Nome);
+Console.WriteLine(carro.Peso);
+Console.WriteLine(carro.Modelo);
+Console.WriteLine(carro.distancia);
